df84bbfeea0233ce351796d360c298994cd38e2e
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2024-12-07 20:08:32 +0200

    switch to window.location in logout

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,7 +16,7 @@
 //todo2: get data from login_data localstorage item, or redirect to index in case of no data in login_data inside localStorage
 
 if (localStorage.getItem("login_data") === null) {
-  window.location.href="../index.html"
+  window.location.href="../index.html";
 } else {
   document.getElementById("user").innerHTML = JSON.parse(
     localStorage.getItem("login_data")
@@ -27,5 +27,6 @@ if (localStorage.getItem("login_data") === null) {
 document.querySelector(".logout").addEventListener("click", function (e) {
   e.preventDefault();
   localStorage.removeItem("login_data");
-  window.open("../index.html");
+  // window.open("../index.html");
+  window.location.href="../index.html";
 });
